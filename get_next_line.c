b1982dab27a74ed9585e556d9ec0f0b290a5736f
lmedeiro <lmedeiro@student.42sp.org.br>
2022-10-07 22:24:15 +0200

    add

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,11 @@
 /*   By: lmedeiro <lmedeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 02:38:26 by lmedeiro          #+#    #+#             */
-/*   Updated: 2022/10/07 02:38:55 by lmedeiro         ###   ########.fr       */
+/*   Updated: 2022/10/07 20:36:05 by lmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //O objetivo é criar uma função que possa retornar uma única linha lida de um descritor 
 //de arquivo, sem perder o rastro da próxima linha e com tamanho de buffer aleatório.
+
+
